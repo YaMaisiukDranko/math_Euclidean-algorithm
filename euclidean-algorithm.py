7dc91ae2d187c41af16eae714060444768836d3c
Yaroslav <yarikmeisan@yandex.com>
2022-11-06 11:04:20 +0300

    Add input to a, b

--- euclidean-algorithm.py
+++ euclidean-algorithm.py
@@ -1,2 +1,10 @@
 #@YaMaisiukDranko
 #Euclidean Algorithm on Python
+
+gcd = int
+#Get a and b
+a = int(input("Write a: "))
+b = int(input("Write b: "))
+print("GCD (", a, ";" ,b, ")")
+
+print(a / b)

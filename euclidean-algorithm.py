a5a5af5a278b24dc4b237dd3c73e2575b3e44899
Yaroslav <yarikmeisan@yandex.com>
2022-11-06 11:31:09 +0300

    Create 0 checking

--- euclidean-algorithm.py
+++ euclidean-algorithm.py
@@ -1,10 +1,13 @@
-#@YaMaisiukDranko
-#Euclidean Algorithm on Python
+# @YaMaisiukDranko
+# Euclidean Algorithm on Python
 
 gcd = int
-#Get a and b
+# Get a, b
 a = int(input("Write a: "))
 b = int(input("Write b: "))
-print("GCD (", a, ";" ,b, ")")
+print("GCD (", a, ";", b, ")")
 
-print(a / b)
+if a % b != 0: #Check for 0
+    print(a % b)
+else:
+    print(a / b)

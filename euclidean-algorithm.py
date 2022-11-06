471eeefd4054049839b7552b0536b1c9354bc76d
Yaroslav <yarikmeisan@yandex.com>
2022-11-06 17:27:12 +0300

    Setting up division in column

--- euclidean-algorithm.py
+++ euclidean-algorithm.py
@@ -1,15 +1,23 @@
 # @YaMaisiukDranko
 # Euclidean Algorithm on Python
 
+import math
+
 a = int(input("What's the first number? "))
 b = int(input("What's the second number? "))
 
 r=a%b
 
 while r:
-    print("a:", a, "b:", b, "r:", r)
+    #print("a:", a, "b:", b, "r:", r)
+    digits = int(math.log10(a)) + 1
+    print(a, "|" + "\033[4m", b, "\033[0m")
+    print("  " * digits, int(a / b))
     a=b
     b=r
     r=a%b
 
+digits = int(math.log10(a)) + 1
+print(a, "|" + "\033[4m", b, "\033[0m")
+print("  " * digits, int(a / b))
 print('GCD is:', b)

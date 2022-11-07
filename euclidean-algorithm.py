c8fe32470f2e7f1efa934b2eca9d860bdf842445
Yaroslav <yarikmeisan@yandex.com>
2022-11-07 15:08:26 +0300

    Set up division in column

--- euclidean-algorithm.py
+++ euclidean-algorithm.py
@@ -10,14 +10,14 @@ r=a%b
 
 while r:
     #print("a:", a, "b:", b, "r:", r)
-    digits = int(math.log10(a)) + 1
+    digits = int(math.log10(a))
     print(a, "|" + "\033[4m", b, "\033[0m")
     print("  " * digits, int(a / b))
     a=b
     b=r
     r=a%b
 
-digits = int(math.log10(a)) + 1
+digits = int(math.log10(a))
 print(a, "|" + "\033[4m", b, "\033[0m")
 print("  " * digits, int(a / b))
 print('GCD is:', b)
